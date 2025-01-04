9145aa9f10c8d8ed97522734d36333c8cf462bdf
Marianna <mariannaniko1092@gmail.com>
2025-01-04 13:45:34 -0500

    Add mainMenu(), viewDepartments() and pg package to index.js

--- index.js
+++ index.js
@@ -1,6 +1,49 @@
-const inquirer = require("inquirer")
-const {printTable} = require('console-table-printer');
-
-
-
+const inquirer = require("inquirer");
+const { printTable } = require("console-table-printer");
+const { Pool } = require("pg");
+require("dotenv").config();
 // view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
+const pool = new Pool(
+    {
+        user: process.env.USER_NAME,
+        password: process.env.PASSWORD,
+        host: "localhost",
+        database: process.env.DBNAME
+    },
+    console.log(`Connected to the employees_db database.`)
+);
+
+pool.connect(() => {
+    mainMenu()
+});
+
+function viewDepartments() {
+    pool.query('SELECT * FROM department', function (err, {rows}) {
+        printTable(rows)
+        mainMenu()
+    })
+}
+function mainMenu() {
+    inquirer.prompt([
+        {
+        type: "list",
+        message: "What would you like to do?",
+        name: "menu",
+        choices: [
+            "view all departments",
+            "view all roles",
+            "view all employees",
+            "add a department",
+            "add a role",
+            "add an employee",
+            "update an employee role",
+            ],
+        },
+    ]).then((response) => {
+        if (response.menu === "view all departments") {
+        viewDepartments();
+        }
+    });
+}
+
+
