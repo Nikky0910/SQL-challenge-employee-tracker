37c41322521762449b7f615711620d8a7b319cb2
Marianna <mariannaniko1092@gmail.com>
2025-01-04 12:44:43 -0500

    Create package.json

---
+++ index.js
@@ -0,0 +1,6 @@
+const inquirer = require("inquirer")
+const {printTable} = require('console-table-printer');
+
+
+
+// view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
